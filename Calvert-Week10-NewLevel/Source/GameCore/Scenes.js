f719ef8f1ab24b1f0cb3d93380cf364f8c6cbf7e
Margie Calvert <margiecalvert@gmail.com>
2013-12-13 23:48:09 -0800

    predefined level

---
+++ Calvert-Week10-NewLevel/Source/GameCore/Scenes.js
@@ -0,0 +1,187 @@
+/**
+ * @author Charlie
+ */
+
+/* jshint browser: true */
+
+// Draw the initial game state
+Crafty.scene('Game', function() { 'use strict';
+
+var board = null;
+var boards = [
+
+        // Level 1
+        [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 4, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]],
+        
+        
+        // Level 2
+        [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1],
+        [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 4, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 4, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]],];
+	
+var createEntity = function(col, row, name) {
+    Crafty.e(name).at(col, row);
+};
+    
+var createVillage = function(col, row){	
+	
+var village= Crafty.e('Village').at(col, row);
+	village.setName(village._entityName.replace('Entity', 'Village'));
+	Crafty.game.newVillage(village);
+};
+
+
+
+var createEntities = function(board) {
+	board = boards[0];
+    for (var x = 0; x < Crafty.game.map_grid.width; x++) {
+        for (var y = 0; y < Crafty.game.map_grid.height; y++) {
+            var gridValue = board[y][x];
+            if (gridValue === 1) {
+                createEntity(x, y, 'Tree');
+            } else if (gridValue === 2) {
+                createEntity(x, y, 'Bush');
+            } else if (gridValue === 3) {
+                createEntity(x, y, 'Food');
+            } else if (gridValue === 4) {
+                createVillage(x, y);
+            }
+        }
+    }
+};
+	
+	
+	this.gameBoard = createEntities(board);
+
+	// Player character, placed at 5, 5 on our grid
+	this.player = Crafty.e('PlayerCharacter').at(5, 5);
+	this.gameBoard[this.player.at().x][this.player.at().y] = true;
+
+	
+    
+	
+	
+	
+
+	// Show the victory screen once all villages are visisted
+	this.show_victory = this.bind('VillageVisited', function() {
+		Crafty.game.sendDebugMessage("Village Length: " + Crafty('Village').length);
+		if (!Crafty('Village').length) {
+			Crafty.scene('Victory');
+		}
+	});
+}, function() { 'use strict';
+	// Remove our event binding from above so that we don't
+	//  end up having multiple redundant event watchers after
+	//  multiple restarts of the game
+	this.unbind('VillageVisited', this.show_victory);
+});
+
+
+// Victory scene : Announce victory, set up a new game
+Crafty.scene('Victory', function() { 'use strict';
+	// Display some text in celebration of the victory
+	Crafty.e('2D, DOM, Text')
+		.attr({ x: 0, y: 0 })
+		.text('You are victorious!');
+	
+		
+
+	// restart the game when a key is pressed
+	this.restart = function() {
+
+		
+		Crafty.scene('Game');
+	};
+
+	// Bind keydown event. This was done wrong in the demo
+	this.bind('KeyDown', this.restart);
+}, function() { 'use strict';
+	// Remove key binding to prevent multiple restarts
+	if (!this.unbind('KeyDown', this.restart)) {
+		window.alert("Could not unbind");
+	}
+
+});
+
+Crafty.scene('Failure', function() { 'use strict';
+	// Display some text in celebration of the victory
+	Crafty.e('2D, DOM, Text')
+		.attr({ x: 0, y: 0 })
+		.text('You Lose!')
+        .textColor('#FFFFFF')
+        .textFont({ size: '48px', weight: 'bold' });
+
+
+	// restart the game when a key is pressed
+	this.restart = function() {
+		Crafty.scene('Game');
+	};
+
+	// Bind keydown event. This was done wrong in the demo
+	this.bind('KeyDown', this.restart);
+}, function() { 'use strict';
+	// Remove key binding to prevent multiple restarts
+	if (!this.unbind('KeyDown', this.restart)) {
+		window.alert("Could not unbind");
+	}
+
+});
+
+// Load binary assets such as images and audio files
+Crafty.scene('Loading', function(){ 'use strict';
+
+	var assets = ['Assets/cscGarden01-32X32.png',
+		'Assets/BoyWalk.png',
+		'Assets/door_knock_3x.mp3',
+		'Assets/People256.png'
+		];
+
+	// Load our sprite map image
+	Crafty.load(assets, function(){
+		Crafty.sprite(32, assets[0], {
+			spr_tree:    [0, 3],
+			spr_bush:    [1, 3],
+			spr_village: [0, 1],
+			spr_food: [0, 0]			
+		});
+
+		//  The main character
+		Crafty.sprite(32, 32, assets[3], {
+			spr_mainCharacter:  [0, 0],
+		}, 0, 0);
+
+		// Define our sounds for later use
+		Crafty.audio.add({
+			knock: ['http://desolate-caverns-4829.herokuapp.com/assets/door_knock_3x.mp3']
+		});
+
+		// Display text while loading
+		Crafty.e('2D, DOM, Text')
+			.attr({ x: 0, y: Crafty.viewport.height / 2 - 24, w: Crafty.viewport.width })
+			.text('Loading...');
+
+		// Now that our sprites are ready to draw, start the game
+		Crafty.scene('Game');
+	});
+});
